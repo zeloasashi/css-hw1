e28d9964aec255acaf000a2093651c5bbead0eb0
Chin <zeloasashi@gmail.com>
2022-11-21 23:23:57 +0800

    test delay

--- index.js
+++ index.js
@@ -3,12 +3,22 @@ $(window).scroll(function(){
     if($(window).scrollTop() >= ($('main').offset().top - window.screen.height * 2 / 3)) {
         $('nav').removeClass('navinvisible').addClass('navbgc')
     }
-    // else{
-    //     $('nav').addClass('navinvisible').removeClass('navbgc')
-    // }
+    else{
+        $('nav').addClass('navinvisible').removeClass('navbgc')
+    }
 })
 
 $('.navbtn').click(function(){
     console.log('nav button click');
-    $('.nav-full').toggleClass('nminvisible')
+    $('.nav-full').toggleClass('nminvisible');
+    $('.nav-menu').toggleClass('trans');
+    $('.m-links').toggleClass('trans');
+})
+$('.navbtn').click(function(){
+    $('nav').toggleClass('navinvisible')
+})
+
+$('.nfbtn').click(function(){
+    console.log('nav-full button click');
+    $('.nav-full').addClass('nminvisible')
 })
